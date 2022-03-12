d521498050dbaeebbd5bc67140dc45eff9ff5a67
RyanLira <101383915+RyanLira@users.noreply.github.com>
2022-03-11 18:03:31 -0600

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,20 @@
+# recursive method for factorial
+'''
+def fact(n):
+    if(n == 1):
+        return 1
+    else:
+        return n*fact(n-1)
+x = int(input("enter input: "))
+result = fact(x)
+print(result)
+'''
+
+#loop method for factorial
+'''
+num = int(input("enter a number: "))
+fac = 1
+for i in range(1, num + 1):
+    fac = fac * i
+print("factorial of ", num, " is ", fac)
+'''
