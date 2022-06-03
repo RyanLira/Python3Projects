779a43acf63f393d3f831d843f36d13e6fc583cf
Ryan Lira <101383915+RyanLira@users.noreply.github.com>
2022-06-02 20:25:45 -0500

    Add files via upload

---
+++ linearSearch.py
@@ -0,0 +1,14 @@
+#Ryan Lira
+#linear search algorthm
+
+##INPUT#########################
+x = [5,7,6,3,8,9,34]
+
+y = input('where is: ')
+
+##PROCESS######################
+i = 0
+while(x[i] != y):
+	i +=1
+
+print(i)
