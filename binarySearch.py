be6c777862cb3421b8c6484235ef6f719d2d0d4e
Ryan Lira <101383915+RyanLira@users.noreply.github.com>
2022-06-14 19:01:31 -0500

    Add files via upload

---
+++ binarySearch.py
@@ -0,0 +1,31 @@
+#binary search
+
+def naive_search(l, target):
+	for i in range(len(l)):
+		if l[i] == target:
+			return i
+	#if its not in there
+	return -1
+
+def binary_search(l, target, low=None, high=None):
+	
+	if low is None:
+		low = 0
+	if high is None:
+		high = len(l) - 1
+	
+	midpoint = (low + high) // 2
+
+	if l[midpoint] == target:
+		return midpoint
+	elif target < l[midpoint]:
+		return binary_search(l, target, low, midpoint-1)
+	else:
+		#target > l[midpoint]
+		return binary_search(l, target, midpoint+1, high)
+
+if __name__=='__main__':
+	l = [1,3,5,10,12]
+	target = 10
+	print(naive_search(l, target))
+	print(binary_search(l, target))
